9d9bd46295cdac3909859a30ad9ad2329c21e3e1
Yogesh Chavan <ychavan@mdsol.com>
2019-12-13 08:17:11 +0530

    removed need of cors

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 // Section 1
 const express = require('express');
 const axios = require('axios');
-const cors = require('cors');
+const path = require('path');
 
 // Section 2
 const app = express();
-app.use(cors());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 // Section 3
 app.get('/', (req, res) => {
