487de00bf7405f9151f086de1267c22e9e01176e
Yogesh Chavan <ychavan@mdsol.com>
2019-12-14 18:37:05 +0530

    optimization changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,38 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.join(__dirname, 'public')
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            use: ['style-loader', 'css-loader'],
-            test: /\.css/
-        }]
-    },
-    mode: 'development',
-    devServer: {
-        contentBase: path.join(__dirname, 'public')
-    }
+module.exports = (env) => {
+    console.log(env);
+    return {
+        entry: './src/app.js',
+        output: {
+            filename: 'bundle.js',
+            path: path.join(__dirname, 'public')
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                      publicPath: path.join(__dirname, 'public')
+                    },
+                  }, 'css-loader'
+                ],
+                test: /\.css/
+            }]
+        },
+        mode: env ? 'production' : 'development',
+        devServer: {
+            contentBase: path.join(__dirname, 'public')
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: 'main.css',
+                chunkFilename: 'main.css',
+            }),
+        ]
+    };
 };
