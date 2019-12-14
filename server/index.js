7bbf07e1cb040026f44b84bd0c6901812748667c
Yogesh Chavan <ychavan@mdsol.com>
2019-12-14 23:56:33 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const axios = require('axios');
 const path = require('path');
 
+const port = process.env.PORT || 3000;
 // Section 2
 const app = express();
 app.use(express.static(path.join(__dirname, '..', 'public')));
@@ -20,6 +21,6 @@ app.get('/users', (req, res) => {
 });
 
 // Section 4
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('server started on port 3000');
 });
